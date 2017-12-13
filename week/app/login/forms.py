82f3633dd0efd8a588722b2552de71525a1dda7c
robin-2016 <xuhongbin3241@gmail.com>
2017-12-13 17:19:19 +0800

    mod username

--- week/app/login/forms.py
+++ week/app/login/forms.py
@@ -1,26 +1,29 @@
 #/bin/python
 #-*-coding=utf-8-*-
-import sys
-reload(sys)
-sys.setdefaultencoding('utf-8')
+# import sys
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
 from flask_wtf import FlaskForm
 from wtforms import validators,SubmitField,StringField,PasswordField,BooleanField,SelectField,ValidationError
-from wtforms.validators import Required,Length,Regexp,EqualTo
+from wtforms.validators import data_required,Length,Regexp,EqualTo
 from ..models import Users,Role,Groups
 
 class LoginForm(FlaskForm):
-	username = StringField("用户名：",[validators.Required()])
-	passwd = PasswordField("密码：",[validators.Required()])
-	remember_me = BooleanField("记住我")
-	submit = SubmitField("登录")
+	username = StringField(u"用户名：",[validators.data_required()])
+	passwd = PasswordField(u"密码：",[validators.data_required()])
+	remember_me = BooleanField(u"记住我")
+	submit = SubmitField(u"登录")
 
 class UseraddForm(FlaskForm):
-	username = StringField("用户名：",validators=[Required(),Length(1,100),Regexp('^[A-Za-z][A-Za-z0-9_.]*$',0,'只能使用字母、数字、下划线和点号！')])
-	passwd = PasswordField("密码：",validators=[Required(),EqualTo('passwd2',message='两次密码不相同！')])
-	passwd2 = PasswordField("确认密码：",validators=[Required()])
+	username = StringField(u"用户名：", validators=[data_required(), Length(1, 100)])
+	# username = StringField(u"用户名：", validators=[data_required(), Length(1, 100),
+	#                                             Regexp('^[\u4e00-\u9fa5]', 0,
+	#                                                    u'只能使用字母、数字、下划线和汉字！')])
+	passwd = PasswordField(u"密码：",validators=[data_required(),EqualTo('passwd2',message=u'两次密码不相同！')])
+	passwd2 = PasswordField(u"确认密码：",validators=[data_required()])
 #	role = SelectField("角色",coerce=int)
-	groups = SelectField("分组",coerce=int)
-	submit = SubmitField('新增用户')
+	groups = SelectField(u"分组",coerce=int)
+	submit = SubmitField(u'新增用户')
 	
 	def __init__(self,*args,**kwargs):
 		super(UseraddForm,self).__init__(*args,**kwargs)
@@ -31,10 +34,10 @@ class UseraddForm(FlaskForm):
 
 	def validate_username(self,field):
 		if Users.query.filter_by(name=field.data).first():
-			raise ValidationError('用户名已经存在！')
+			raise ValidationError(u'用户名已经存在！')
 
 class ChangepwForm(FlaskForm):
-	passwdold = PasswordField("旧密码：",[validators.Required()])
-	passwd = PasswordField("新密码：",validators=[Required(),EqualTo('passwd2',message='两次密码不相同！')])
-	passwd2 = PasswordField("确认密码：",validators=[Required()])
-	submit = SubmitField('更改密码')
+	passwdold = PasswordField(u"旧密码：",[validators.data_required()])
+	passwd = PasswordField(u"新密码：",validators=[data_required(),EqualTo('passwd2',message=u'两次密码不相同！')])
+	passwd2 = PasswordField(u"确认密码：",validators=[data_required()])
+	submit = SubmitField(u'更改密码')
